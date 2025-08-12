856ef553a7baaebac76fd9040ea3d98ec0c8ff9f
Arya Kakodkar <kakodkararya@gmail.com>
2025-08-12 15:44:54 +0100

    Add utils functionality

--- utils/ct_utils.jl
+++ utils/ct_utils.jl
@@ -1,6 +1,10 @@
 module CTUtils
 
+
+## Functions for comparing SCAPS calculations and ChargeTransport calculations
 export scaps_to_df
+export ct_to_df
+export compare_densities
 
 include("scaps_utils.jl")
 
--- utils/scaps_utils.jl
+++ utils/scaps_utils.jl
@@ -1,4 +1,6 @@
 using DataFrames
+using CSV
+using PyPlot
 
 """
 Convert a SCAPS file to a DataFrame.
@@ -27,4 +29,36 @@ function scaps_to_df(filename::String)
     df = DataFrame([getindex.(data, i) for i in 1:length(header)], Symbol.(header))
 
     return df
+end
+
+"""
+Convert a SCAPS file to a DataFrame.
+"""
+function ct_to_df(filename::String)
+    df = CSV.read(filename, DataFrame)
+
+    df.x .= df.x .* 1e6  # Convert x from cm to um
+    df.n .= df.n .* 1e-6  # Convert n from /cm3 to /um3
+    df.p .= df.p .* 1e-6  # Convert x from cm to um
+    return df
+end
+
+
+"""
+Plots carrier density values from SCAPS and ChargeTransport
+"""
+function compare_densities(df_ct::DataFrame, df_scaps::DataFrame)
+    # Plotting
+    figure(figsize=(10, 6))
+    plot(df_ct[!, "x"], df_ct[!, "n"], label="CT (n)", color="blue")
+    plot(df_scaps[!, "x(um)"], df_scaps[!, "n(/cm3)"], label="SCAPS (n)", color="blue", linestyle="--")
+    plot(df_ct[!, "x"], df_ct[!, "p"], label="CT (p)", color="red")
+    plot(df_scaps[!, "x(um)"], df_scaps[!, "p(/cm3)"], label="SCAPS (p)", color="red", linestyle="--")
+
+    yscale("log")
+    xlabel("x(um)")
+    ylabel("n(/cm3)")
+    title("Comparison of Carrier Densities")
+    legend()
+    grid(true)
 end
